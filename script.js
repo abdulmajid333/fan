eb7accc6968b5f9052995e20d0a2a92d8783e4b3
majid <majidchoudhary879@gmail.com>
2024-06-23 12:48:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+let fanimg = document.getElementById('fanclip');
+
+function on(){
+    fanimg.style.animationDuration = 3 + "s";
+}
+
+function off(){
+    fanimg.style.animationDuration = 0 + "s";
+}
+
+function speed1(){
+    fanimg.style.animationDuration = 1 + "s";
+}
+
+function speed2(){
+    fanimg.style.animationDuration = 0.5 + "s";
+}
+
+function speed3(){
+    fanimg.style.animationDuration = 0.1 + "s";
+}
